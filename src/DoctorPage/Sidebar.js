fe560412c0d6e7bb07f901dec340f31ac0bdaf55
Selman <selmanebrahim567@gmail.com>
2025-09-11 00:22:21 +0300

    remove unused import2

--- src/DoctorPage/Sidebar.js
+++ src/DoctorPage/Sidebar.js
@@ -7,15 +7,7 @@ function Sidebar() {
       <div className="sidebar-header">
         Doctor Home
       </div>
-      <ul className="sidebar-menu">
-        <li><a href="#">Profile</a></li>
-        <li><a href="#">To-Do List</a></li>
-        <li><a href="#">Patients</a></li>
-        {/* Add more custom items here */}
-        <li><a href="#">Appointments</a></li>
-        <li><a href="#">Reports</a></li>
-        <li><a href="#">Settings</a></li>
-      </ul>
+      
     </div>
   );
 }
